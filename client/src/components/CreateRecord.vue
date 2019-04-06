d1464c653e261d132ee897845fc3f2e7641023e3
mehedi094030 <mehedi094030@gmail.com>
2019-04-06 19:23:58 +0600

    Null Validation

--- client/src/components/CreateRecord.vue
+++ client/src/components/CreateRecord.vue
@@ -9,8 +9,20 @@
 			></v-text-field>
 		</v-flex>
 		<v-flex xs4>
-			<v-btn class="mt=2" dark color="green" @click="$emit('create')">
+			<v-btn 
+				class="mt=2" 
+				dark color="green" 
+				@click="$emit('create')"
+				v-if="value"
+			>
 				<v-icon class="mr-2">add_circle</v-icon>
+				Create
+			</v-btn>
+			<v-btn
+				class="mt=2" 
+				v-if="!value"
+				disabled
+			>
 				Create
 			</v-btn>
 		</v-flex>
